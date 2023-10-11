bdd0be415ffeda41075b2d5210bb777c9781e0e5
Андрей Серебренников <a.serebrennikov1986@gmail.com>
2023-10-11 10:09:45 +0500

    added a directory src  with the file cli.js

---
+++ src/cli.js
@@ -0,0 +1,10 @@
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+
+const greeting = () => {
+    const userName = readlineSync.question('May I have your name? ');
+    return(`Hello, ${userName}!`);
+};
+console.log(greeting());
+export default greeting;
