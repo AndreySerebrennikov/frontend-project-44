c641379ff949e23178dc12556f39944677cafb86
Андрей Серебренников <a.serebrennikov1986@gmail.com>
2023-10-11 11:42:04 +0500

    fixed linter errors

--- src/cli.js
+++ src/cli.js
@@ -3,8 +3,8 @@ import readlineSync from 'readline-sync';
 console.log('Welcome to the Brain Games!');
 
 const greeting = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    return(`Hello, ${userName}!`);
+  const userName = readlineSync.question('May I have your name? ');
+  return (`Hello, ${userName}!`);
 };
 console.log(greeting());
 export default greeting;
