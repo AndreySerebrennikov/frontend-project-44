f75b1014abb41154bc944d717e79712334b4266c
Андрей Серебренников <a.serebrennikov1986@gmail.com>
2023-10-18 12:18:01 +0500

    fix bin/brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { questions } from '../src/games/brainCalc.js';
+import questions from '../src/games/brainCalc.js';
 import { greeting } from '../src/index.js';
 
 greeting();
