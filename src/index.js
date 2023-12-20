7cc2c2ce7bb59fde12715210642fd13b64c6c062
Андрей Серебренников <a.serebrennikov1986@gmail.com>
2023-12-20 18:13:45 +0500

    refactor index.js and utils.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import readlineSync from 'readline-sync';
 
-// eslint-disable-next-line import/no-mutable-exports
-export let userName;
-export const greeting = () => {
+const runLogicGame = (ruleGame, getRoundGame) => {
   console.log('Welcome to the Brain Games!');
-  userName = readlineSync.question('May I have your name? ');
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
+  console.log(ruleGame);
+
+  const roundCount = 3;
+  for (let i = 0; i < roundCount; i += 1) {
+    const [question, correctAnswer] = getRoundGame();
+    const answerUser = readlineSync.question(`Question: ${question}\nYor answer: `);
+    if (correctAnswer !== answerUser) {
+      console.log(`${answerUser} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${userName}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${userName}!`);
 };
+export default runLogicGame;
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
-const getRandomNum = (max, min) => Math.floor(Math.random() * (max - min));
+const getRandomNum = (max, min) => Math.floor(Math.random() * (max - min) + min);
 const getRandomIndex = () => Math.floor(Math.random() * 10);
 export { getRandomNum, getRandomIndex };
