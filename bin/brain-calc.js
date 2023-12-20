240e584ae0eaea9fc2bea0b1d9c4f1ce307a8f50
Андрей Серебренников <a.serebrennikov1986@gmail.com>
2023-12-20 19:07:37 +0500

    refactor brainCalc.js and brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-import questions from '../src/games/brainCalc.js';
-import { greeting } from '../src/index.js';
+import runBarinCalc from '../src/games/brainCalc.js';
 
-greeting();
-questions();
+runBarinCalc();
--- src/games/brainCalc.js
+++ src/games/brainCalc.js
@@ -1,48 +1,38 @@
-import readlineSync from 'readline-sync';
-import { userName } from '../index.js';
+import runLogicGame from '../index.js';
+import { getRandomNum } from '../utils.js';
 
-let resultExpression;
-let answerUser;
+const ruleGame = 'What is the result of the expression?';
+const maxNumber = 100;
+const minNumber = 1;
 
-const expression = () => {
-  const arr = ['+', '-', '*'];
-  const randomZnak = Math.random();
-  const randomNum1 = Math.floor(Math.random() * 10);
-  const randomNum2 = Math.floor(Math.random() * 10);
+const getOperator = () => {
+  const operators = ['+', '-', '*'];
+  const index = Math.floor(Math.random() * operators.length);
+  return operators[index];
+};
 
-  const i = Math.floor(randomZnak * arr.length);
-  switch (i) {
-    case 0:
-      resultExpression = `${randomNum1 + randomNum2}`;
-      return (`${randomNum1} + ${randomNum2}`);
-    case 1:
-      resultExpression = `${randomNum1 - randomNum2}`;
-      return (`${randomNum1} - ${randomNum2}`);
-    case 2:
-      resultExpression = `${randomNum1 * randomNum2}`;
-      return (`${randomNum1} * ${randomNum2}`);
+const getAnswerExpression = (number1, number2, operator) => {
+  switch (operator) {
+    case '+':
+      return number1 + number2;
+    case '-':
+      return number1 - number2;
+    case '*':
+      return number1 * number2;
     default:
-      return ('');
+      return ' ';
   }
 };
 
-const question = () => {
-  answerUser = readlineSync.question(`Question: ${expression()}\nYor answer: `);
-  return (resultExpression === answerUser);
+const getRoundGame = () => {
+  const number1 = getRandomNum(maxNumber, minNumber);
+  const number2 = getRandomNum(maxNumber, minNumber);
+  const operator = getOperator();
+
+  const question = `${number1} ${operator} ${number2}`;
+  const correctAnswer = getAnswerExpression(number1, number2, operator).toString();
+  return [question, correctAnswer];
 };
 
-const questions = () => {
-  console.log('What is the result of the expression?');
-  for (let i = 0; i < 3; i += 1) {
-    const resultQuestion = question();
-    if (resultQuestion) {
-      console.log('Correct!');
-    } else {
-      console.log(`${answerUser} is wrong answer ;(. Correct answer was ${resultExpression}.\nLet's try again, ${userName}!`);
-      return '';
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
-  return '';
-};
-export default questions;
+const runBarinCalc = () => runLogicGame(ruleGame, getRoundGame);
+export default runBarinCalc;
