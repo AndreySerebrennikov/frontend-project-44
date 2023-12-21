971c7c9fc2cc64555c93fcb3140126e56df2f7d2
Андрей Серебренников <a.serebrennikov1986@gmail.com>
2023-12-21 14:23:56 +0500

    fix src/utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,2 @@
 const getRandomNum = (max, min) => Math.floor(Math.random() * (max - min) + min);
-const getRandomIndex = () => Math.floor(Math.random() * 10);
-export { getRandomNum, getRandomIndex };
+export default getRandomNum;
