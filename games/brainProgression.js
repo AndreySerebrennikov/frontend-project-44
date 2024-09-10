3de822353895901b1998d35119681c1b13274436
Андрей Серебренников <a.serebrennikov1986@gmail.com>
2024-09-10 14:19:48 +0500

    added brainProgression.js

---
+++ games/brainProgression.js
@@ -0,0 +1,35 @@
+import executeLogicGame, { getRandomNumber } from '../src/index.js';
+
+const getListNumbers = (startSequence, sizeSequence) => {
+  const numbers = [];
+  let currentNumber = startSequence;
+  for (let i = 0; i < 10; i += 1) {
+    currentNumber += sizeSequence;
+    numbers.push(currentNumber);
+  }
+  return numbers.sort((a, b) => a - b);
+};
+
+const getSequenceHiddenNumber = (sequenceNumbers, indexHiddenNumber) => {
+  const numbers = sequenceNumbers;
+  numbers[indexHiddenNumber] = '..';
+  return numbers.join(' ');
+};
+
+const ruleGame = 'What number is missing in the progression?';
+const runRoundGame = () => {
+  const sizeSequence = getRandomNumber(2, 8);
+  const startSequence = getRandomNumber(1, 100);
+  const indexHiddenNumber = Math.floor(Math.random() * 10);
+
+  const sequenceNumbers = getListNumbers(startSequence, sizeSequence);
+  const hiddenNumber = sequenceNumbers[indexHiddenNumber];
+  const sequenceHiddenNumber = getSequenceHiddenNumber(sequenceNumbers, indexHiddenNumber);
+
+  const question = `Question: ${sequenceHiddenNumber}`;
+  const correctAnswer = hiddenNumber;
+  return [question, correctAnswer];
+};
+
+const runBrainProgression = () => executeLogicGame(ruleGame, runRoundGame);
+export default runBrainProgression;
